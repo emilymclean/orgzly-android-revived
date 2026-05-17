a954024f90b560e9bb5de201371121d013632f52
Emily McLean <emilymcleandeveloper@gmail.com>
2026-05-18 09:18:27 +1000

    Animate in and out refine search button

--- app/src/main/java/com/orgzly/android/ui/notes/query/SearchFilterScaffold.kt
+++ app/src/main/java/com/orgzly/android/ui/notes/query/SearchFilterScaffold.kt
@@ -1,11 +1,20 @@
 package com.orgzly.android.ui.notes.query
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -19,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import cl.emilym.compose.units.rdp
@@ -48,27 +58,36 @@ fun SearchFilterScaffold(
         Box(Modifier.fillMaxSize()) {
             content()
 
+            AnimatedVisibility(
+                showRefineButton,
+                Modifier.align(Alignment.BottomCenter),
+                enter = slideInVertically(
+                    initialOffsetY = { it }
+                ),
+                exit = slideOutVertically(
+                    targetOffsetY = { it }
+                )
+            ) {
+                OrgzlyButton(
+                    onClick = {
+                        sheetVisible = true
+                    },
+                    Modifier
+                        .padding(horizontal = 1.rdp)
+                        .padding(bottom = 1.rdp)
+                        .scaffoldPadding(contentPadding)
+                ) {
+                    Text(
+                        stringResource(R.string.query_filter_search)
+                    )
+                }
+            }
+
             Box(
                 Modifier
                     .fillMaxSize()
                     .scaffoldPadding(contentPadding)
             ) {
-                if (showRefineButton) {
-                    OrgzlyButton(
-                        onClick = {
-                            sheetVisible = true
-                        },
-                        Modifier
-                            .align(Alignment.BottomCenter)
-                            .padding(horizontal = 1.rdp)
-                            .padding(bottom = 1.rdp)
-                    ) {
-                        Text(
-                            stringResource(R.string.query_filter_search)
-                        )
-                    }
-                }
-
                 filter?.let { filter ->
                     if (sheetVisible) {
                         ModalBottomSheet(
